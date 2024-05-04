83c4e19e9493025010b4838d40d12c1147a04592
doquocphong <doquocphong321@gmail.com>
2024-05-05 00:18:57 +0200

    commit frontend

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+export const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+
+        <div className="descriptionbox-description">
+            <p>
+                Move in comfort or lounge in comfort. These adidas leggings have you covered no matter what your day looks like. Stretchy cotton with nylon mesh lining hugs your body, 
+                celebrating every curve. Embroidery gives a shout to your love of classic adidas trainers. Our cotton products support more sustainable cotton farming.
+                Move in comfort or lounge in comfort. These adidas leggings have you covered no matter what your day looks like. Stretchy cotton with nylon mesh lining hugs your body, 
+                celebrating every curve. Embroidery gives a shout to your love of classic adidas trainers. Our cotton products support more sustainable cotton farming.
+                Move in comfort or lounge in comfort. These adidas leggings have you covered no matter what your day looks like. Stretchy cotton with nylon mesh lining hugs your body, 
+                celebrating every curve. Embroidery gives a shout to your love of classic adidas trainers. Our cotton products support more sustainable cotton farming.
+            </p>
+            <p>
+                Move in comfort or lounge in comfort. These adidas leggings have you covered no matter what your day looks like. Stretchy cotton with nylon mesh lining hugs your body, 
+                celebrating every curve. Embroidery gives a shout to your love of classic adidas trainers. Our cotton products support more sustainable cotton farming.
+            </p>
+        </div>
+    </div>
+  )
+}
